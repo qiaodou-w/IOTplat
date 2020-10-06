54c00cf1668ecf093ce9cc75c6716e8ca920f02f
Eeran <wyr990204@163.com>
2020-10-06 17:03:44 +0800

    add login page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import EnvDetail from "../views/EnvDetail";
+import Login from "../views/Login";
 
 Vue.use(VueRouter);
 
@@ -10,6 +11,11 @@ const routes = [
     path: "/",
     redirect: "/home"
   },
+	{
+		path: "/login",
+		name: 'Login',
+		component: Login
+	},
   {
     path: "/home",
     name: "Home",
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,49 @@
 <template>
-$END$
+  <div>
+    <van-form @submit="onSubmit">
+      <van-field
+          v-model="user.username"
+          name="username"
+          label="用户名"
+          placeholder="用户名"
+          :rules="[{ required: true, message: '请填写用户名' }]"
+      />
+      <van-field
+          v-model="user.password"
+          type="password"
+          name="password"
+          label="密码"
+          placeholder="密码"
+          :rules="[{ required: true, message: '请填写密码' }]"
+      />
+      <div style="margin: 16px;">
+        <van-button round block type="info" native-type="submit">
+          提交
+        </van-button>
+      </div>
+    </van-form>
+  </div>
 </template>
 
 <script>
+import Vue from 'vue';
+import { Form, Field } from 'vant';
+
+Vue.use(Form);
+Vue.use(Field);
 export default {
-name: "Login"
+  name: "Login",
+  data() {
+    return {
+      user: {}
+    }
+  },
+  methods: {
+    onSubmit(values) {
+      console.log('submit', this.user);
+      console.log( values )
+    },
+  },
 }
 </script>
 
